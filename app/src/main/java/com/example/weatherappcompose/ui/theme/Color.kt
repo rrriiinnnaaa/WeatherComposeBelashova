b91e835a4f8ce8d1df2d64aeaef910bd0af086c7
unknown <arinalikesnevra@gmail.com>
2024-10-19 11:57:53 +0900

    weathercompose

---
+++ app/src/main/java/com/example/weatherappcompose/ui/theme/Color.kt
@@ -0,0 +1,12 @@
+package com.example.weatherappcompose.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFB1E5FA)
+val PurpleGrey80 = Color(0xFFB7D9E4)
+val Pink80 = Color(0xFF618592)
+
+val Purple40 = Color(0xFF508CA4)
+val PurpleGrey40 = Color(0xFF5B6B71)
+val Pink40 = Color(0xFF526E7D)
+val Grey00 = Color(0xFF97A7AF)
